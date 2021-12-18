900320182d827a43d7b2bd76768e017df45b721f
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-12-18 16:01:57 -0300

    test deploy 29

--- api/src/db.js
+++ api/src/db.js
@@ -6,34 +6,34 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize;
 
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    dialect: "postgres",
-    // host: DB_HOST,
-    // port: 5432,
-    // pool: {
-    //   max: 3,
-    //   min: 1,
-    //   idle: 10000,
-    // },
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    //   keepAlive: true,
-    // },
-    // ssl: true,
-  });
-} else {
-  sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    }
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//     dialect: "postgres",
+//     // host: DB_HOST,
+//     // port: 5432,
+//     // pool: {
+//     //   max: 3,
+//     //   min: 1,
+//     //   idle: 10000,
+//     // },
+//     // dialectOptions: {
+//     //   ssl: {
+//     //     require: true,
+//     //     rejectUnauthorized: false,
+//     //   },
+//     //   keepAlive: true,
+//     // },
+//     // ssl: true,
+//   });
+// } else {
+sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
+// }
 
 const basename = path.basename(__filename);
 
