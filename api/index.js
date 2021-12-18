2b2bc4c7ee266ff249b50a1abe584da33c14d668
lautaromugas8 <lautaro_mugas@hotmail.com>
2021-12-18 15:08:25 -0300

    test deploy 20

--- api/index.js
+++ api/index.js
@@ -21,11 +21,10 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 const PORT = process.env.PORT ?? 3001;
-const HOST = process.env.HOST ?? "0.0.0.0";
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, HOST, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening on port ${PORT}`); // eslint-disable-line no-console
   });
 });
