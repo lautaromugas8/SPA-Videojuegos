57d4f47cfb3d178e753ff4a05dbcd937e980ab8c
lautaromugas8 <lautaro_mugas@hotmail.com>
2022-01-12 22:29:01 -0300

    updated packages

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,6 @@ import "./index.css";
 import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
